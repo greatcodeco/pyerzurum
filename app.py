da42db0d02ea4e935c50d35bbe3e02dc1a211bc6
Ali Ilteris Keskin <aliilteriskeskin@gmail.com>
2019-05-21 20:34:39 +0300

    Yeni site tasarımı eklendi

--- app.py
+++ app.py
@@ -3,9 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'linuxdegilgnulinux'
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/participation')
+def participation():
+    return render_template('katilim.html')
+
+@app.route('/about')
+def about():
+    return render_template('hakkinda.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('iletisim.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
